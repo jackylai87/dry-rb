7a051b734a7e948ac683f602b66f94b90b3f81cb
jackylai87 <jackylai87@gmail.com>
2017-10-14 12:44:49 +0800

    checkpoint 1

--- lib/types.rb
+++ lib/types.rb
@@ -3,4 +3,11 @@ require "dry-types"
 
 module Types
   include Dry::Types.module
+
+  ArticleStatus = Strict::String.default('draft').enum('draft', 'published')
 end
+
+# constant to be available everywhere within app
+# Usage
+	# require 'types'
+	# Types::ArticleStatus.values
