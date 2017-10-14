387dd957e347f94825d64d5ec4faf968e96659be
jackylai87 <jackylai87@gmail.com>
2017-10-14 16:22:09 +0800

    checkpoint 2

--- apps/admin/lib/admin/article_repo.rb
+++ apps/admin/lib/admin/article_repo.rb
@@ -3,5 +3,18 @@ require "admin/entities"
 
 module Admin
   class ArticleRepo < Blog::Repository[:articles]
+  	relations :authors
+
+  	commands :create
+
+  	struct_namespace Admin::Entities
+
+  	def by_pk(id)
+  		articles.by_pk(id).one
+  	end
+
+  	def listing
+  		articles.combine(:author).order_by_created_at
+  	end
   end
 end
--- lib/persistence/relations/articles.rb
+++ lib/persistence/relations/articles.rb
@@ -1,7 +1,17 @@
 module Persistence
   module Relations
     class Articles < ROM::Relation[:sql]
-      schema :articles, infer: true
+      schema :articles, infer: true do
+      	associations do
+      		belongs_to :author
+      	end
+      end
+
+      def order_by_created_at
+      	# order(self[:created_at].desc)
+
+      	order { created_at.desc }
+      end
     end
   end
 end
